5cbfa5fbeccb5f50da897987f2e0367767a29516
Michael Joseph Ojeda <mikelojeda25@gmail.com>
2026-05-18 14:13:17 +0800

    chore: downloaded lucide and framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
+import React from 'react'
+import Hero from './components/Hero'
+
 function App() {
   return (
-    <div className="min-h-screen bg-blue-50 flex items-center justify-center">
-      <div className="bg-white rounded-2xl shadow-lg p-10 text-center">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">
-          Steafunny Jean Nepangue Cotejo
-        </h1>
-        <p className="text-gray-500">Client Services Assistant</p>
-        <button className="my-6 px-6 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition cursor-pointer">
-          CONTACT ME
-        </button>
-
-        <p className="text-gray-500">
-          IG: cotejosteafunny | Mobile: 09758558715 |Telegram: 09758558715 | Viber: 09758558715 
-        </p>
-      </div>
-    </div>
+    <Hero />
   )
 }
 
